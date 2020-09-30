5bb0f875dc33a1736034b1fb2b4ac4ad232f6395
Chris Bautista <CBaut@users.noreply.github.com>
2020-09-30 08:49:48 -0700

    algos

---
+++ week1/day3/algos.js
@@ -0,0 +1,70 @@
+/*
+  Given an arr and a separator string, output a string of every item in the array separated by the separator.
+
+  No trailing separator at the end
+  Default the separator to a comma with a space after it if no separator is provided
+*/
+
+// var arr1 = [1, 2, 3]
+// var separator1 = ", "
+// var expected1 = "1, 2, 3"
+
+// var arr2 = [1, 2, 3]
+// var separator2 = "-"
+// var expected2 = "1-2-3"
+
+// var arr3 = [1, 2, 3]
+// var separator3 = " - "
+// var expected3 = "1 - 2 - 3"
+
+// var arr4 = [1]
+// var separator4 = ", "
+// var expected4 = "1"
+
+// var arr5 = []
+// var separator5 = ", "
+// var expected5 = ""
+
+function join(arr, separator) {
+  // code here
+}
+
+/* ******************************************************************************** */
+
+/*
+  String Encode
+
+  You are given a string that may contain sequences of consecutive characters.
+  Create a function to shorten a string by including the character,
+  then the number of times it appears.
+
+  */
+
+// var str1 = "aaaabbcddd"
+// var expected1 = "a4b2c1d3"
+
+// var str2 = ""
+// var expected2 = ""
+
+// var str3 = "aa"
+// var expected3 = "a2"
+
+// var str4 = "bbcc"
+// var expected4 = "b2c2"
+
+function encodeStr(str) {
+  // code here
+}
+
+/* ******************************************************************************** */
+
+/*
+  String Decode
+*/
+
+// var str1 = "a3b2c1d3"
+// var expected1 = "aaabbcddd"
+
+function decodeStr(str) {
+  // code here
+}
