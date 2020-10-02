1d5445dcc9951a907d33d31084941c494f1a67ee
Chris Bautista <CBaut@users.noreply.github.com>
2020-10-01 17:54:10 -0700

    afternoon

--- week1/day4/demo_project/test_app/urls.py
+++ week1/day4/demo_project/test_app/urls.py
@@ -3,11 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # these paths render templates
     path('', views.index),
     path('thursday', views.show_thursday),
-    path('redirect', views.demo_redirect),
-
     path('day/<str:some_day>', views.display_day),
+
+    # these paths return redirects
+    path('redirect', views.demo_redirect),
     # path('day/tuesday', views.demo_redirect),
     # path('day/wendesday', views.demo_redirect),
 ]
--- week1/day4/demo_project/test_app/views.py
+++ week1/day4/demo_project/test_app/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import HttpResponse, redirect, render
 
 
-# Create your views here.
+# These functions render templates
 def index(request):
+    print(request)
     print('We hit the index function!')
     # do some magic!
-
 
     # return HttpResponse('<h1>hello</h1>')
     return render(request, 'index.html')
@@ -14,12 +14,6 @@ def show_thursday(request):
     print('Thursday function!')
     return render(request, 'thursday.html')
 
-def demo_redirect(request):
-    # some processing of the request
-    # CRUD
-    return redirect('/thursday')
-
-
 def display_day(request, some_day):
     print('Display Day function!')
     print(some_day)
@@ -27,6 +21,12 @@ def display_day(request, some_day):
       "the_day": some_day,
       "name": "Zach",
       "favorite_color": "turquoise",
-      "pets": ["Bruce", "Fitz", "Georgie", "Bud"]
+      "pets": ["Bruce", "Fitz", "Georgie", "Bud", "Bruce", "Fitz", "Georgie", "Bud","Bruce", "Fitz", "Georgie", "Bud","Bruce", "Fitz", "Georgie", "Bud","Bruce", "Fitz", "Georgie", "Bud","Bruce", "Fitz", "Georgie", "Bud","Bruce", "Fitz", "Georgie", "Bud","Bruce", "Fitz", "Georgie", "Bud","Bruce", "Fitz", "Georgie", "Bud","Bruce", "Fitz", "Georgie", "Bud","Bruce", "Fitz", "Georgie", "Bud","Bruce", "Fitz", "Georgie", "Bud","Bruce", "Fitz", "Georgie", "Bud","Bruce", "Fitz", "Georgie", "Bud","Bruce", "Fitz", "Georgie", "Bud","Bruce", "Fitz", "Georgie", "Bud","Bruce", "Fitz", "Georgie", "Bud","Bruce", "Fitz", "Georgie", "Bud","Bruce", "Fitz", "Georgie", "Bud","Bruce", "Fitz", "Georgie", "Bud", "Bruce", "Fitz", "Georgie", "Bud","Bruce", "Fitz", "Georgie", "Bud","Bruce", "Fitz", "Georgie", "Bud","Bruce", "Fitz", "Georgie", "Bud","Bruce", "Fitz", "Georgie", "Bud","Bruce", "Fitz", "Georgie", "Bud","Bruce", "Fitz", "Georgie", "Bud","Bruce", "Fitz", "Georgie", "Bud","Bruce", "Fitz", "Georgie", "Bud","Bruce", "Fitz", "Georgie", "Bud","Bruce", "Fitz", "Georgie", "Bud","Bruce", "Fitz", "Georgie", "Bud","Bruce", "Fitz", "Georgie", "Bud"]
     }
     return render(request, 'day.html', context)
+
+# These functions processes requests then redirect
+def demo_redirect(request):
+    # some processing of the request
+    # CRUD
+    return redirect('/thursday')
